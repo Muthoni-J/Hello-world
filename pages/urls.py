bd83ba1d81ee8636a02a85f1fb05ad1e17bd157f
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-07-15 19:17:18 +0300

    changes

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView,AboutPageView,ServicesPageView,ContactUsPageView
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('About/', AboutPageView.as_view(), name='about'),
+    path('Services/', ServicesPageView.as_view(), name='services'),
+    path('Contact Us/', ContactUsPageView.as_view(), name='contact us')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+from django.views.generic import TemplateView
+class HomePageView(TemplateView): template_name = 'Home.html'
+class AboutPageView(TemplateView): template_name = 'About.html'
+class ServicesPageView(TemplateView): template_name = 'Services.html'
+class ContactUsPageView(TemplateView): template_name = 'Contact Us.html'
